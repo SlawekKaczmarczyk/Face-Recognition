cf2c8d7577c0f8c650c9bdc80a0eff68134cc94c
SlawekKaczmarczyk <slawomir.kaczmarczyk1998@gmail.com>
2023-05-07 13:19:51 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+def encoded_faces():
+    # Look through faces and encode 
+    pass
+
+def unknown_image_encoded():
+    # Encode a face
+    pass
+
+def classify():
+    # Find all faces in image, label them and display results
+    #
+    pass
+
